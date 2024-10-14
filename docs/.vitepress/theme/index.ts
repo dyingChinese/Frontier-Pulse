0a8eedb988d74b56e47b9542345b07b12f9fa4f0
indragon <mawenlong@datagrand.com>
2024-10-14 22:02:18 +0800

    feature(ArticleLayout)- 添加文章归档索引页 todo 添加必要的样式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,33 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import type { Component } from "vue";
+import DefaultTheme from "vitepress/theme";
+import ArticleLayout from "./layout/articleLayout.vue";
+import "./style.css";
+import DefaultLayout from "./layout/DefaultLayout.vue";
+
+interface IModulesType {
+  [k: string]: Component;
+}
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: DefaultLayout,
+    // return h(DefaultTheme.Layout, null, {
+    // // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    // })
+    // DefaultLayout: DefaultTheme.Layout,
+    // ArticleLayout: ArticleLayout,
+  
   enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-} satisfies Theme
+    // console.log(app,router);
+    const modules:IModulesType[] = import.meta.glob("./layout/*.vue",{ eager: true });
+
+    for (const [key, component] of Object.entries(modules)) {
+      app.component(key, component);
+    }
+
+    // app.component("ArticleLayout", ArticleLayout);
+  },
+} satisfies Theme;
