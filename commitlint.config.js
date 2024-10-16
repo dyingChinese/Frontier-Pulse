67faa623c82a9300cb73d5b2e5db5734b48fb674
dyingChinese <62784051+dyingChinese@users.noreply.github.com>
2024-10-16 23:27:42 +0800

    chore: various changes to project configuration
    - add commitlint.config.js
    - fix package.json
    - add script

--- commitlint.config.js
+++ commitlint.config.js
@@ -45,25 +45,25 @@ export default {
     },
     // prettier-ignore
     types: [
-            {value: "feat", name: "特性:     ✨  新增功能", emoji: ":sparkles:"},
-            {value: "fix", name: "修复:     🐛  修复缺陷", emoji: ":bug:"},
-            {value: "docs", name: "文档:     📝  文档变更", emoji: ":memo:"},
-            {value: "style", name: "格式:     💄  代码格式（不影响功能，例如空格、分号等格式修正）", emoji: ":lipstick:"},
-            {value: "refactor", name: "重构:     ♻️  代码重构（不包括 bug 修复、功能新增）", emoji: ":recycle:"},
-            {value: "perf", name: "性能:     ⚡️  性能优化", emoji: ":zap:"},
-            {value: "test", name: "测试:     ✅  添加疏漏测试或已有测试改动", emoji: ":white_check_mark:"},
-            {
-                value: "build",
-                name: "构建:     📦️  构建流程、外部依赖变更（如升级 npm 包、修改 vite 配置等）",
-                emoji: ":package:"
-            },
-            {value: "ci", name: "集成:     🎡  修改 CI 配置、脚本", emoji: ":ferris_wheel:"},
-            {value: "revert", name: "回退:     ⏪️  回滚 commit", emoji: ":rewind:"},
-            {
-                value: "chore",
-                name: "其他:     🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）",
-                emoji: ":hammer:"
-            },
+        {value: "-feat", name: "特性:     ✨  新增功能", emoji: "✨"},
+        {value: "-fix", name: "修复:     🐛  修复缺陷", emoji: "🐛"},
+        {value: "-docs", name: "文档:     📝  文档变更", emoji: "📝"},
+        {value: "-style", name: "格式:     💄  代码格式（不影响功能，例如空格、分号等格式修正）", emoji: "💄"},
+        {value: "-refactor", name: "重构:     ♻️  代码重构（不包括 bug 修复、功能新增）", emoji: "♻️"},
+        {value: "-perf", name: "性能:     ⚡️  性能优化", emoji: "⚡️"},
+        {value: "-test", name: "测试:     ✅  添加疏漏测试或已有测试改动", emoji: "✅"},
+        {
+            value: "-build",
+            name: "构建:     📦️  构建流程、外部依赖变更（如升级 npm 包、修改 vite 配置等）",
+            emoji: "📦️"
+        },
+        {value: "-ci", name: "集成:     🎡  修改 CI 配置、脚本", emoji: "🎡"},
+        {value: "-revert", name: "回退:     ⏪️  回滚 commit", emoji: "⏪️"},
+        {
+            value: "-chore",
+            name: "其他:     🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）",
+            emoji: "🔨"
+        },
         ],
     useEmoji: true,
     emojiAlign: 'center',
